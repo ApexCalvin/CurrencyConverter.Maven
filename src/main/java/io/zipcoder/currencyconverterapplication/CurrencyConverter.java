1e8a89616bb2f9c03bc6ba06fcc950bf2a204880
ApexCalvin-WinOS <ApexCalvin@gmail.com>
2022-11-29 21:10:18 -0500

    testing windows OS

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -4,4 +4,13 @@ public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
     }
+
+    //Testing changes
+    public static Double convert1(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
+        return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+    }
+
+    public static Double convert2(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
+        return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+    }
 }
